c4d18d899004e434826cc871c6cf63cac6a3ae81
Minchul An <min@minchulan.io>
2023-04-06 14:46:04 -0600

    commit tree ab7824456aa65cbda407ab8ced91950e1738e805

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  #index 
   get '/students', to: 'students#index'
+
+  #show 
+  get '/students/:id', to: 'students#show'
+
+  #grades 
   get '/students/grades', to: 'students#grades'
+
+  #highest_grade 
   get '/students/highest-grade', to: 'students#highest_grade'
 end
